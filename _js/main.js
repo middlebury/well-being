ac3afbb511d4935572bdad9790f498cdc5bb50ee
Zeb Pykosz <zeb.apy@gmail.com>
2016-11-10 12:29:30 -0500

    refactor swapper to use classes to hide and show

--- _js/main.js
+++ _js/main.js
@@ -2,57 +2,15 @@ require('babel-polyfill');
 
 const Swapper = require('./swapper');
 
-class Toggler {
-  constructor(selector) {
-    this.elem = document.querySelector(selector);
-    this.display = this.getStyle();
-  }
-
-  hide() {
-    this.elem.style.display = 'none';
-  }
-
-  show() {
-    this.elem.style.display = null;
-  }
-
-  getStyle() {
-    var element = this.elem;
-    return element.currentStyle
-      ? element.currentStyle.display
-      : getComputedStyle(element, null).display;
-  }
-}
-
-const intro = new Toggler('.topics__intro');
-const controls = new Toggler('.controls');
-const listBubble = document.querySelector('.topics-list .bubble');
-
 const swapper = new Swapper({
+  itemsContainer: '.topics-list',
   navItems: '.topics-nav__anchor',
   items: '.topic-article',
-  onOpen(id) {
-    intro.hide();
-    controls.show();
-    listBubble.classList.add('active');
-    listBubble.style.display = null;
-
-    // window.history.pushState({
-    //   title: id,
-    // }, id, `/topics/#${id}`);
-  },
-  onClose() {
-    intro.show();
-    // show(intro);
-    controls.hide();
-    listBubble.classList.remove('active');
-    listBubble.style.display = 'none';
-  }
 });
 
-var id = window.location.hash.replace('#', '');
+// var id = window.location.hash.replace('#', '');
 
-swapper.show(id);
+// swapper.show(id);
 
 var closeBtn = document.querySelector('.controls__btn--close');
 var prevBtn = document.querySelector('.controls__btn--prev');
--- _js/swapper.js
+++ _js/swapper.js
@@ -2,23 +2,36 @@ class Swapper {
   constructor({
     items,
     navItems,
+    itemsContainer,
     onOpen = () => null,
     onClose = () => null,
-    activeClassName = 'active'
+    activeNavItemClass = 'swapper-nav-item--is-active',
+    activeItemClass = 'swapper-item--is-active',
+    activeContainerClass = 'swapper-container--is-init',
+    openContainerClass = 'swapper-container--is-open',
+    closedContainerClass = 'swapper-container--is-closed',
+    openBodyClass = 'swapper--is-open',
+    closedBodyClass = 'swapper--is-closed',
   }) {
     this.navItems = document.querySelectorAll(navItems);
     this.items = document.querySelectorAll(items);
+    this.container = document.querySelector(itemsContainer);
     this.onOpen = onOpen;
     this.onClose = onClose;
-    this.activeClassName = activeClassName;
-    this.currIndex = 0;
-    this.ids = [];
-    Array.from(this.items).forEach(elem => this.ids.push(elem.id));
+    this.activeNavItemClass = activeNavItemClass;
+    this.activeItemClass = activeItemClass;
+    this.activeContainerClass = activeContainerClass;
+    this.openContainerClass = openContainerClass;
+    this.closedContainerClass = closedContainerClass;
+    this.openBodyClass = openBodyClass;
+    this.closedBodyClass = closedBodyClass;
   }
 
   init() {
     this.closeAll();
     this.addListeners();
+    this.container.classList.add(this.activeContainerClass);
+    document.body.classList.add('has-swapper');
   }
 
   addListeners() {
@@ -38,51 +51,69 @@ class Swapper {
     }
   }
 
-  setCurrIndex(id) {
-    this.currIndex = this.ids.indexOf(id);
-  }
-
   show(id) {
     if(!id) return;
     this.closeAll();
     this.onOpen(id);
     const elem = document.getElementById(id);
-    elem.style.display = 'block';
-    this.setCurrIndex(id);
-    this.addActiveClass(id);
+    elem.classList.add(this.activeItemClass);
+    this.addActiveNavItemClass(id);
+    this.container.classList.add(this.openContainerClass);
+    this.container.classList.remove(this.closedContainerClass);
+    document.body.classList.add(this.openBodyClass);
+  }
+
+  getActiveNavItem() {
+    return document.querySelector('.' + this.activeNavItemClass);
+  }
+
+  getActiveItem() {
+    return this.container.querySelector('.' + this.activeItemClass);
   }
 
   closeAll() {
+    const elem = this.getActiveItem();
+    if(elem) {
+      elem.classList.remove(this.activeItemClass);
+    }
+
+    document.body.classList.remove(this.openBodyClass);
+
+    this.removeActiveNavItemClass();
+
+    // run onClose callback
     this.onClose();
-    Array.from(this.items).forEach(elem => {
-      elem.style.display = 'none';
-    });
-    this.removeActiveClasses();
   }
 
-  addActiveClass(id) {
+  addActiveNavItemClass(id) {
     const elem = document.querySelector(`[href="#${id}"]`);
-    this.removeActiveClasses();
-    elem.classList.add(this.activeClassName);
+    this.removeActiveNavItemClass();
+    elem.classList.add(this.activeNavItemClass);
   }
 
-  removeActiveClasses() {
-    Array.from(this.navItems).forEach(elem => {
-      elem.classList.remove(this.activeClassName);
-    });
+  removeActiveNavItemClass() {
+    const elem = this.getActiveNavItem();
+    if(elem) {
+      elem.classList.remove(this.activeNavItemClass);
+    }
   }
 
   next() {
-    const nextIndex = this.currIndex + 1;
-    const index = nextIndex === this.ids.length ? 0 : nextIndex;
-    const id = this.ids[index];
+    const { nextElementSibling } = this.getActiveItem();
+    if(nextElementSibling && nextElementSibling.id) {
+      return this.show(nextElementSibling.id);
+    }
+    var id = this.items[0].getAttribute('id');
     this.show(id);
   }
 
   prev() {
-    const prevIndex = this.currIndex - 1;
-    const index = prevIndex < 0 ? this.ids.length - 1 : prevIndex;
-    const id = this.ids[index];
+    const { previousElementSibling } = this.getActiveItem();
+    if(previousElementSibling && previousElementSibling.id) {
+      return this.show(previousElementSibling.id);
+    }
+    const lastItem = this.items[this.items.length - 1];
+    const id = lastItem.getAttribute('id');
     this.show(id);
   }
 }
