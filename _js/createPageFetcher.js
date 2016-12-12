825120db5847ef526789966e6e4de4382a09a158
Zeb Pykosz <zeb.apy@gmail.com>
2016-12-12 17:04:57 -0500

    start adding anime for spinner

--- _js/createPageFetcher.js
+++ _js/createPageFetcher.js
@@ -4,19 +4,36 @@ const PageFetcher = require('./helpers/PageFetcher');
 // if previous state is overview, then the user is navigating home
 const isUrlOverview = url => url.indexOf('overview') >= 0;
 
+const SPINNER_DURATION = 500;
+const ROOT_DURATION = 500;
+
 module.exports = function createPageFetcher({ afterChange }) {
   const root = document.getElementById('root');
+  const spinner = document.getElementById('spinner');
 
   return new PageFetcher({
     root,
     beforeChange: (url, cb) => {
+      // animate the root div out
       anime({
         targets: root,
         opacity: 0,
-        duration: 500,
+        duration: ROOT_DURATION,
         translateY: isUrlOverview(url) ? 20 : -20,
         easing: 'linear',
-        complete: cb
+        complete: () => {
+
+          // show spinner
+          spinner.style.display = 'block';
+
+          // animate the spinner
+          anime({
+            targets: spinner,
+            opacity: [0, 1],
+            duration: SPINNER_DURATION,
+            complete: cb
+          });
+        }
       });
     },
     afterChange: (url) => {
@@ -24,15 +41,25 @@ module.exports = function createPageFetcher({ afterChange }) {
       // App needs to re-run initHelpers to re-instance the swapper or video background
       afterChange();
 
+      // hide spinner
       anime({
-        targets: root,
-        duration: 500,
-        delay: 100,
-        opacity: 1,
-        translateY: isUrlOverview(url) ? [-20, 0] : [20, 0],
-        easing: 'linear',
+        targets: spinner,
+        duration: SPINNER_DURATION,
+        opacity: 0,
         complete: () => {
-          root.removeAttribute('style');
+
+          // show root container
+          anime({
+            targets: root,
+            duration: ROOT_DURATION,
+            delay: 100,
+            opacity: 1,
+            translateY: isUrlOverview(url) ? [-20, 0] : [20, 0],
+            easing: 'linear',
+            complete: () => {
+              root.removeAttribute('style');
+            }
+          });
         }
       });
     }
