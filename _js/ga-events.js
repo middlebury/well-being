b58f41676bd10af42fbbf9bdf94530c877e6eb84
Zeb Pykosz <zeb.apy@gmail.com>
2017-01-24 12:51:35 -0500

    add GA event tagging

---
+++ _js/ga-events.js
@@ -0,0 +1,27 @@
+var forEach = require('./helpers/forEach');
+
+var links = document.querySelectorAll('[data-ga-event]');
+
+function handleLinkClick(event) {
+  var target = event.currentTarget;
+  var hitType = 'event';
+  var eventCategory = 'Links';
+  var eventAction = 'click';
+  var eventLabel = target.dataset.gaEvent;
+
+  var config = {
+    hitType,
+    eventCategory,
+    eventAction,
+    eventLabel
+  };
+
+  if(typeof window.ga !== 'undefined') {
+    return window.ga('send', config);
+  }
+  console.log('logging GA event', config);
+}
+
+forEach(links, link => {
+  link.addEventListener('click', handleLinkClick);
+});
--- _js/main.js
+++ _js/main.js
@@ -2,6 +2,8 @@ if (typeof Promise === 'undefined') {
   window.Promise = require('promise/lib/es6-extensions.js');
 }
 
+require('./ga-events');
+
 var html = document.documentElement;
 html.classList.add('js');
 html.classList.remove('no-js');
